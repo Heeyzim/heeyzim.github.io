cece487d7f7c8864808259e089827afb33f915fe
Luigi Zampieri <zampieri.luigi17@gmail.com>
2025-01-18 13:39:03 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import { ThemeProvider } from './components/theme-provider'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className='h-screen w-full flex flex-col gap-4 items-center justify-center'>
+        <h1>Luigi Zampieri</h1>
+        
+        <Button asChild>
+          <a href="https://github.com/heeyzim" target='_blank'>GitHub</a>
+        </Button>
+        </div>
+    </ThemeProvider>
+  )
+}
+
+export default App

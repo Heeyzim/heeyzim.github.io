033edd5f87104a558962072023131fd7c5d416c5
Luigi Zampieri <zampieri.luigi17@gmail.com>
2025-01-18 13:56:53 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className='h-screen w-full flex flex-col gap-4 items-center justify-center'>
-        <h1>Luigi Zampieri</h1>
+        <h1>Luigi Zampieri =)</h1>
         
         <Button asChild>
           <a href="https://github.com/heeyzim" target='_blank'>GitHub</a>
